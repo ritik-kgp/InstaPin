9c283ea6d225965effc7beb81ec9ea6a4a8cbc32
Ritik Chaturvedi <ritikchaturvedi2104@gmail.com>
2023-10-26 21:53:52 +0530

    aded Login feature

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <GoogleOAuthProvider
+    clientId={`${process.env.REACT_PUBLIC_GOOGLE_API_TOKEN}`}
+  >
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
